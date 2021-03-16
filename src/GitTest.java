b3eb8b15ab6dfe627b523b0043e776012ba1c57a
techtorial <50930535+techtorial@users.noreply.github.com>
2021-03-16 18:46:28 -0500

    Test
    Test

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,5 +2,6 @@ public class GitTest{
     public static void main(String[] args){
         System.out.println("100_000.00$");
         System.out.println("This is new Project");
+        System.out.println("This");
     }
 }
